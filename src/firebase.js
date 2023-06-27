0c01a93336cf6cce8e128661cb65b87e408f7b50
Animish_Sharma <animishharitash@gmail.com>
2023-06-27 17:41:56 +0530

    first-commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKoXNLnfTOPdqYoqu-mGho2dov81Nz6WU",
+  authDomain: "anichat-1f4ba.firebaseapp.com",
+  projectId: "anichat-1f4ba",
+  storageBucket: "anichat-1f4ba.appspot.com",
+  messagingSenderId: "179419858725",
+  appId: "1:179419858725:web:97c6e40ae8e419fac87ccc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore()
